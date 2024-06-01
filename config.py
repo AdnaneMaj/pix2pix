16b945e3e7e6ee3b2191c10c8263ec35930e2c6f
MAJDOUB Adnane <52354033+AdnaneMaj@users.noreply.github.com>
2024-06-01 21:05:04 +0100

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import albumentations as A
 from albumentations.pytorch import ToTensorV2
 
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
-LEARNING_RATE = 2e-4
+LEARNING_RATE = 1e-4
 BATCH_SIZE = 16
 NUM_WORKERS = 2
 IMAGE_SIZE = 512
@@ -17,8 +17,7 @@ CHECKPOINT_DISC = "/kaggle/working/disc.pth.tar"
 CHECKPOINT_GEN = "/kaggle/working/gen.pth.tar"
 
 both_transform = A.Compose(
-    [A.Resize(width=512, height=512),
-    A.Rotate(limit=(0, 360), p=1.0),], additional_targets={"image0": "image"},
+    [A.Resize(width=512, height=512),], additional_targets={"image0": "image"},
 )
 
 transform_only_input = A.Compose(
